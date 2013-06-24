2ed0e695019d54982968a2bf8f791915c8b03a43
shaunwong <shaun.kirk.wong@gmail.com>
2013-06-24 16:41:16 -0700

    added xml checks for stream xml via lodash

--- update.js
+++ update.js
@@ -6,21 +6,39 @@
 */ 
 
 // dependencies
-var reqxml = require('./utils/reqxml.js');
+var reqxml = require( './utils/reqxml.js' ),
+	_ = require( 'lodash' );
 
 // config
 var options = ({
-	'headers': {
-		'Accept-Encoding': 'gzip'
-	},
-	url:'http://www.teamliquid.net/video/streams/?xml=1'
+  'headers': {
+    'Accept-Encoding': 'gzip'
+  },
+  url: 'http://www.teamliquid.net/video/streams/?xml=1'
 });
 
 // request xml
-reqxml(options, function(result){
-	update(result);
+reqxml( options, function( result ) {
+  update( result );
 });
 
-function update(streams){
-	
+function update( streams ) {
+  
+  // check for stream format
+  if( _( streams ).has( 'streamlist' ) ) {
+		if( _( streams.streamlist ).has( 'stream' ) ) {
+			console.log( 'xml list found' );
+		} else {
+			console.log( 'xml error: stream not found.' );
+			return false;
+		} 
+  } else {
+	  console.log( 'xml error: streamlist not found.' );
+	  return false;
+  }
+  
+  // iterate over streams
+  _( streams.streamlist.stream ).each( function( s ) {
+		console.log ( s );
+  });
 }
--- utils/reqxml.js
+++ utils/reqxml.js
@@ -6,53 +6,54 @@
 */ 
 
 // dependencies
-var	request = require('request'),
-		zlib = require('zlib'),
-		parseString = require('xml2js').parseString;
+var request = require( 'request' ),
+	zlib = require( 'zlib' ),
+	parseString = require( 'xml2js' ).parseString;
 
-var reqxml = function(options,callback) {
-	
-	// make a request
-	req = function(options) {
-		var res = request(options, function (err,res,body) {
-			if (!err){
-				return body;
-			} else {
-				console.log(err);
-				//return new Error(err);
-			}
-		});
-		zip(res);
-	},
-	
-	// gunzip response
-	zip = function(res) {
-		var	gunzip = zlib.createGunzip(), xml = "";
-				
-		gunzip.on('data', function(data) {
-			xml += data.toString();
-		}); 
-	
-		gunzip.on('end', function() {
-			parse(xml);
-		});
-		res.pipe(gunzip);
-	},
+var reqxml = function( options, callback ) {
+  
+  // make a request
+  req = function( options ) {
+    var res = request( options, function ( err, res, body ) {
+      if ( !err ){
+        return body;
+      } else {
+        console.log( err );
+        //return new Error(err);
+      }
+    });
+    zip( res );
+  },
+  
+  // gunzip response
+  zip = function( res ) {
+    var  gunzip = zlib.createGunzip(), 
+    	xml = '';
+        
+    gunzip.on( 'data', function( data ) {
+      xml += data.toString();
+    }); 
+  
+    gunzip.on( 'end', function() {
+      parse( xml );
+    });
+    res.pipe( gunzip );
+  },
 
-	// parse xml
-	parse = function(xml){
-		parseString(xml, function(err,result) {
-				if (!err) {
-					callback(result);	
-				}else{
-					console.log(err);
-					//return new Error(err);
-				}
-		});	
-	},
-	
-	//init
-	index = req(options)	
+  // parse xml
+  parse = function( xml ){
+    parseString( xml, function( err, result ) {
+        if ( !err ) {
+          callback( result );  
+        } else {
+          console.log( err );
+          //return new Error(err);
+        }
+    });  
+  },
+  
+  // init
+  index = req( options ); 
 };
 
 module.exports = reqxml;
